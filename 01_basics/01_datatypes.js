f1fc7c662befa13ba73604cf228ffec34d2871e9
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-10 11:50:19 +0000

    array and objects

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -4,4 +4,16 @@ console.log(typeof("Pratyush"));
 console.log(typeof(null));
 console.log(typeof(undefined));
 
+let pirates=["Luffy","Shanks","Zoro"]; // Arrays
+
+let King={
+    Name :"Luffy",
+    bounty: "5 Billion",
+}    //Objects......
+
+console.log(pirates);
+console.log(King.Name);
+console.log(King.bounty);
+
+
 
