115442a960542a955135126893bb2b60786f006d
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-10 10:40:36 +0000

    comparisions

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,24 @@
+/*console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+*/
+
+/*console.log("2">1);//js converts 2 into number and compares....
+console.log("02">1);
+*/
+/*
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+*/
+
+/*
+console.log(undefined==0); //false
+console.log(undefined>=0); //false
+console.log(undefined<=0); //false
+*/
+
+//strict check...
+console.log("1"==1); // converts and check,,,,
+console.log("1"===1); //strictly checks (Also checks the data type)
