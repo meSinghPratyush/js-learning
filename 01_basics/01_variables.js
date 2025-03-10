91318627937bd5d7ee26e974698a9705b62adfe0
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-10 09:24:25 +0000

    01_Variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=2470237
+let accountMail="uic.19bca1418@gmail.com"
+var accountPassword="12345"
+accountCity="Patna"
+
+//accountId=2470233
+accountMail="yamalgoat5@gmail.com"
+accountPassword="1808"
+accountCity="Jehanabad"
+let accountState;
+//console.log(accountId);
+console.table([accountId,accountMail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var because of scope issue in JS....
+ */
