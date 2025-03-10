5028d2bdf9190457c3e35db18083bb110ae5cbba
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-10 10:27:51 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -4,4 +4,15 @@ let valueinNum=Number(score)  //conversion
 console.log(typeof(valueinNum)); //number
 console.log(valueinNum);   //33
 
+//***************Operations*******************
+
+/*console.log("1"+2+2); -122
+console.log(1+2+"3"); - 33
+console.log("1"+(3+4)); - 17
+*/
+
+
+
+
+
 
