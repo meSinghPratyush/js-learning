09859a944a82e491023fb6b8585b2274eee06982
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-11 12:07:35 +0000

    maths and num

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,37 @@
+/// *****Numbers****//
+/*
+const balance = new Number(100)
+console.log(balance.toString().length); //3
+console.log(balance.toFixed(2)); //100.00
+
+const othernum=33.3333
+
+console.log(othernum.toPrecision(3)); //33.3
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+*/
+
+// ********Maths*********//
+
+console.log(Math.abs(-4)); //4
+console.log(Math.abs(4.5)); //4.5
+console.log(Math.round(4.5)); //5
+console.log(Math.floor(4.8));//4
+console.log(Math.ceil(4.2)); //5
+console.log(Math.sqrt(64)); //8
+console.log(Math.pow(2,8)); //256
+
+const random=Math.random();
+console.log(random);
+
+const min=1
+const max=6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min); // formula for having random values between an interval (Dice roll)....
+
+
+
+
+
+
