eec5d99dbaa45d6c0003ec81b13a4e9fdca2aa99
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-11 11:07:07 +0000

    Strings

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,9 +1,10 @@
 "use strict";
 //alert(3+3) // As we are using node js not browser..
+/*
 console.log(typeof("Pratyush"));
 console.log(typeof(null));
 console.log(typeof(undefined));
-
+*/
 //refrence or non premitive type.... 
 // Arrays
 
@@ -14,10 +15,14 @@ let King={
     Name :"Luffy",
     bounty: "5 Billion",
 }    
-
+/*
 console.log(pirates);
 console.log(King.Name);
 console.log(King.bounty);
+*/
+
+// Stack and heap memory............
+
 
 
 
---
+++ 01_basics/Strings.js
@@ -0,0 +1,33 @@
+const name="Luffy"
+const bounty="5 Billion berry"
+//console.log(`Hello my name is ${name} and my bounty is ${bounty}.`);
+ 
+//declaring a string in a diffrent way....
+//Note- This way of declaring string gives us access to diffrent kind of methods and functions...
+
+const gameName=new String("RDR3-2025")
+/*
+console.log(gameName.length); // 4
+console.log(gameName.toLowerCase()); //rdr2
+console.log(gameName.charAt(2));//R
+console.log(gameName.indexOf('R'));//0
+console.log(gameName[3]); // 2
+*/
+//Substring and slicing...
+/*
+const substr=gameName.substring(0,3) //gives a substring between this range..
+console.log(substr);
+
+const slice2 = gameName.slice(-4,-1)//202
+console.log(slice2);
+*/
+
+//trip & replace/....
+
+//Trim - Removes all starting and end spaces...
+
+const king= "       I am Monkey D Luffy.        "
+console.log(king.trim());
+console.log(king);
+
+
