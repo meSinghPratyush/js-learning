927b77cc2c16c3171dbd436b3ea02331d9e52188
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-12 11:50:50 +0000

    arrays 2

--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -19,7 +19,7 @@ myarr.shift() // Removes the first element from the array.
 //console.log(myarr.join()); //Binds array and converts it into string..
 
 //Slice and splice..
-
+/*
 const arr1=[0,1,2,3,4,5]
 console.log("A",arr1);
 
@@ -30,6 +30,47 @@ console.log("B",arr1);
 const mnarr2=arr1.splice(1,3)
 console.log("C",arr1);
 console.log(mnarr2);
+*/
+
+//more methods..
+
+//merging two arrays....
+
+//1> Concat.
+const strawhats=["Luffy","Zoro","sanji"]
+const Redhaired=["Shanks","bekmann","Yasoop"]
+
+const Goats=strawhats.concat(Redhaired);
+
+//console.log(Goats);
+
+//2> Spread.
+
+const Goats1=[...strawhats,...Redhaired]
+//console.log(Goats1);
+
+//**************converting diffrent types into array****************...
+
+//console.log(Array.isArray("Luffy")); // false - not an array
+//console.log(Array.from("Luffy")); // converts to array.
+const myinfo={
+    name:"Pratyush",
+    Roll: "2470237"
+}
+//console.log(Array.from(myinfo.name));
+//console.log(Array.from(myinfo.Roll));
+
+//************Storing multiple elements into one array ***************
+
+let score1=100;
+let score2=200;
+let score3=300;
+let score4=400;
+console.log(Array.of(score1,score2,score3,score4));
+
+
+
+
 
 
 
