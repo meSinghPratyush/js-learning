b03895c49661d4cf0e8ad38bf803714b1dbdf84b
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-16 11:07:12 +0000

    Function1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+function addition(num1,num2){
+    return num1+num2
+}
+const sum=addition(3,4)
+//console.log(sum);
+
+function loginusermsg(username="Unnamed user"){
+    if(!username){
+        console.log("Please enter a username");
+        
+    }
+    else{
+        return `${username} Just logged in`
+    }
+    
+}
+
+//console.log(loginusermsg("Pratyush"));
+
+//Shopping cart problem...
+
+function calculateCartPrice(...price){
+    return price
+}
+//console.log(calculateCartPrice(20,200,2000,400));
+
+ //handling object in function....
+
+ const user={
+    username:"Pratyush",
+    userID:"2470237"
+ }
+ function userDetails(object1){
+    console.log(`Username is : ${object1.username} and userID is :${object1.userID}`);
+    
+ }
+
+ //userDetails(user);
+
