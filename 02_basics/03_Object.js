386673bedab052e8e0aeb30a9af2e8c9d26ef867
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-16 10:20:30 +0000

    Object and API(JSON talks)

---
+++ 02_basics/03_Object.js
@@ -0,0 +1,30 @@
+// Destructuring of objects..
+/*
+What is destructuring?
+-> 
+The destructuring syntax is a JavaScript syntax that makes it possible to unpack values from arrays, or properties from objects, into distinct variables. It can be used in locations that receive data (such as the left-hand side of an assignment or anywhere that creates new identifier bindings).
+*/
+
+//Another way to get values from an object....
+
+const course={
+    name:"JS in hindi",
+    price:"999",
+    instructor:"Hitesh"
+}
+
+const {instructor:Newinstructor}=course
+
+//console.log(Newinstructor);
+
+//Little about API....
+
+//JSON.....
+{
+
+}
+
+
+[
+
+]
