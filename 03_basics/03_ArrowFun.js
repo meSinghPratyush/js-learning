42970ab04406829a9004f4d3d52a7a744587f619
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-17 12:27:23 +0000

    Arrayfun syntax

--- 03_basics/03_ArrowFun.js
+++ 03_basics/03_ArrowFun.js
@@ -4,10 +4,21 @@ const user={
     Username:"Pratyush",
     price:999,
     welcomemessage:function(){
-        console.log(`${this.Username}, Welcome to website`);
+        //console.log(`${this.Username}, Welcome to website`);
     }
 }
 
-user.welcomemessage()
-user.Username="Jimmy"
-user.welcomemessage()
+//user.welcomemessage()
+//user.Username="Jimmy"
+//user.welcomemessage()
+
+//Arrow function...
+/*
+const addtwo=(num1,num2)=>{
+    return num1+num2;
+}*/
+//console.log(addtwo(4,6))
+
+//implict return...
+
+const addtwo=(num1,num2)=> (num1+num2) //no use of return keyword..
