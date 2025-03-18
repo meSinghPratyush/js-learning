352e92b34e3c1e86a05ec73907bd75b7b1b0b7c2
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-18 11:41:30 +0000

    forof loop

---
+++ 05_loops/moreloops.js
@@ -0,0 +1,40 @@
+//for of loop..
+
+const arr=[1,2,3,4,5,6]
+for (const num of arr) {
+    //console.log(num);
+    
+}
+
+const greetings="Hello nerds"
+
+for (const word of greetings) {
+    if(word===' '){
+        continue
+    }
+    else{
+        //console.log(`Each character is :${word}`);
+    }
+    
+}
+//maps datatype...
+
+const map=new Map()
+map.set('IN',"INDIA")
+map.set('USA',"UNITED STATES OF AMERICA")
+map.set('FN',"FRANCE")
+map.set('SP',"SPAIN")
+
+for (const [key,value] of map) {
+   // console.log(key," -: ",value);
+    
+}
+
+const myobj={
+    'Game1':"NFSMW",
+    'GAME2':"SPIDERMAN"
+}
+for (const [key,game] of myobj) {
+    console.log(key," -: ",game); //not iteratable using for-of loop
+    
+}
