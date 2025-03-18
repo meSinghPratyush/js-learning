a10e1b98d94c6138200d9eda1b999e68e5ee9ff6
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-18 11:03:06 +0000

    loops

---
+++ 05_loops/loops.js
@@ -0,0 +1,21 @@
+/*for(let i=1;i<=10;i++){
+    console.log(i);
+}
+*/    
+/*
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+*/
+let arr=[10,20,30,40,50,60,70,80]
+/*for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}*/    
+
+i=0;
+do{
+    console.log(arr[i]);
+    i++;
+}while(i<=0)
