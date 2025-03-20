2776fcd006023baf4720921837d41867b1d5c259
Singh Pratyush <96429431+meSinghPratyush@users.noreply.github.com>
2025-03-20 17:29:21 +0000

    reduce function

--- 05_loops/08.js
+++ 05_loops/08.js
@@ -1,2 +1,12 @@
 //reduce method....
 
+const arr=[1,2,3,4]
+// const total=arr.reduce(function (acc,currval){
+//     return acc+currval
+// },0)
+
+// console.log(total);
+
+const total=arr.reduce((acc,currval) =>{return acc+currval},0)
+console.log(total);
+
